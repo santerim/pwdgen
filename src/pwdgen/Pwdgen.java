36371c49c994981a62c901c728b2d40258af7e30
Santeri Martikainen <kalle.martikainen@gmail.com>
2018-04-09 04:50:38 +0300

    init

---
+++ src/pwdgen/Pwdgen.java
@@ -0,0 +1,18 @@
+
+
+package pwdgen;
+
+/**
+ *
+ * @author Santeri
+ */
+public class Pwdgen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
