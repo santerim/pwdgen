fe888318cb20a4fd1636c92b7b7a5547e132c4e9
Santeri Martikainen <kalle.martikainen@gmail.com>
2018-04-09 19:35:06 +0300

    clipboard copy

--- src/pwdgen/Pwdgen.java
+++ src/pwdgen/Pwdgen.java
@@ -1,6 +1,8 @@
 package pwdgen;
 
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -16,26 +18,10 @@ public class Pwdgen {
      */
     public static void main(String[] args) {
         final PasswordGenerator pg = new PasswordGenerator();
-//        System.out.println(pg.Generate(15));
-//        final JFrame frame = new JFrame();
-//        final JOptionPane pane = new JOptionPane();
-//        String input = pane.showInputDialog(frame, "Give password length (1-50)", 25);
-//        int inputInt = 0;
-//
-//        try {
-//            inputInt = Integer.parseInt(input);
-//        } catch (NumberFormatException error) {
-//            System.out.println(error);
-//        }
-//
-//        if (inputInt > 0 && inputInt < 50) {
-//            String pwd = pg.Generate(inputInt);
-//            System.out.println("PWD: " + pwd);
-//        }
 
         JFrame frame = new JFrame("pwdgen");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 300);
+        frame.setSize(550, 300);
         
         JPanel panel = new JPanel(new GridBagLayout());
 //        panel.setMinimumSize(new Dimension(400, 300));
@@ -46,10 +32,10 @@ public class Pwdgen {
         JLabel labelLengthInfo = new JLabel("Length of password (8-50 characters)");
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.gridwidth = 1;
+//        gbc.gridwidth = 1;
         gbc.ipadx = 95;
-        gbc.insets = new Insets(0, 0, 20, 0);
-        gbc.weightx = 0.9;
+        gbc.insets = new Insets(0, 15, 15, 0);
+        gbc.weightx = 0.7;
 //        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.anchor = GridBagConstraints.EAST;
         panel.add(labelLengthInfo, gbc);
@@ -57,42 +43,55 @@ public class Pwdgen {
         JTextField inputTextField = new JTextField(2);
         gbc.gridx = 1;
         gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.insets = new Insets(0, 0, 20, 0);
+//        gbc.gridwidth = 1;
+        gbc.insets = new Insets(0, 15, 15, 15);
         gbc.weightx = 0.1;
         gbc.anchor = GridBagConstraints.WEST;
 //        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(inputTextField, gbc);
         
         JButton genButton = new JButton("Generate");
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridwidth = 2;
-        gbc.insets = new Insets(0, 0, 15, 0);
-        gbc.weightx = 0;
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+//        gbc.gridwidth = 2;
+        gbc.insets = new Insets(0, 0, 15, 15);
+        gbc.weightx = 0.2;
         gbc.anchor = GridBagConstraints.CENTER;
 //        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(genButton, gbc);
         
-        JLabel outputTextField = new JLabel(" ", SwingConstants.CENTER);
+        JLabel outputTextField = new JLabel("No password generated yet", SwingConstants.CENTER);
+//        outputTextField.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 //        outputTextField.setBounds(new Rectangle(0, 0, 500, 10));
         gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridwidth = 2;
-        gbc.insets = new Insets(10, 0, 0, 0);
+        gbc.gridy = 1;
+        gbc.gridwidth = 3;
+        gbc.insets = new Insets(10, 0, 10, 0);
 //        gbc.anchor = GridBagConstraints.PAGE_END;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+//        gbc.ipadx = 550;
         panel.add(outputTextField, gbc);
         
+        JButton copyButton = new JButton("Copy");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.insets = new Insets(10, 0, 0, 0);
+        gbc.fill = GridBagConstraints.CENTER;
+        gbc.anchor = GridBagConstraints.PAGE_END;
+        panel.add(copyButton, gbc);
+        
+        // ActionListener for password generation button
         genButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent ae) {
                 String pwd = "";
                 try{
                     int length = Integer.parseInt(inputTextField.getText());
+                    System.out.println(length);
                     if(length < 1) {
                         outputTextField.setText("Fill in the desired password length");
                     }
-                    if(length > 0 && length < 51) {
+                    else if(length > 0 && length < 51) {
                         pwd = pg.Generate(length);
 
                         outputTextField.setText(pwd);
@@ -110,6 +109,16 @@ public class Pwdgen {
             }
         });
 
+        copyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                StringSelection ss = new StringSelection(outputTextField.getText());
+                Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+                cb.setContents(ss, null);
+                outputTextField.setText("Copied to clipboard");
+        }
+        });
+        
         frame.add(panel);
 //        frame.pack();
         frame.setVisible(true);
